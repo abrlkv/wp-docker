e03b2dab0e11db5f9438685bea760a9c326705fc
Alex Burlakov <alex@mryorsh.org>
2016-11-02 21:55:28 +0800

    sudo for install composer

--- run.sh
+++ run.sh
@@ -32,8 +32,8 @@ docker-compose &> /dev/null
 
 if [[ $? -eq 127 ]]; then
     echo "Get and install docker-compose"
-    curl -L "https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose
-    chmod +x /usr/local/bin/docker-compose
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
 fi
 
 tar &> /dev/null #check availability tar
